c889b40ac645e5713625fec8872c57710e4d2a2e
Min <85483551+min-coding@users.noreply.github.com>
2022-11-26 14:28:54 +0700

    set router,page,nav,bootstrap

--- chat-frontend/src/App.js
+++ chat-frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import Chat from './pages/Chat';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navigation></Navigation>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/chat" element={<Chat />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
