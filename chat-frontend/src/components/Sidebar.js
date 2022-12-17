268f194463effce1dbbce9d6984d54d8149a5221
Min <85483551+min-coding@users.noreply.github.com>
2022-12-17 15:28:39 +0700

    last commit on redux_toolkit branch test
    I just merged before committing this. Yikes okay let's see what happens

--- chat-frontend/src/components/Sidebar.js
+++ chat-frontend/src/components/Sidebar.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { ListGroup } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 
 export default function Sidebar() {
   const rooms = ['firstroom', 'secondroom', 'thirdroom'];
+  const user = useSelector((state) => state.user);
+
+  if (!user) {
+    return <></>;
+  }
+
   return (
     <>
       <h2> Available Room</h2>
