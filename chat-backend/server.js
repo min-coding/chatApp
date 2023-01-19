2b6b50c8e54b3ea5e7223b6c415aebc5bf102982
Min <85483551+min-coding@users.noreply.github.com>
2023-01-19 23:03:23 +0700

    local perfect

--- chat-backend/server.js
+++ chat-backend/server.js
@@ -19,20 +19,16 @@ const server = require('http').createServer(app);
 const PORT = 5001;
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'https://chatapp-gjpw.onrender.com/',
+    origin: ['http://localhost:3000'],
     method: ['GET', 'POST'],
   },
 });
 
-const dirname = path.resolve();
-app.use(express.static(path.join(dirname, '/chat-frontend/build')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(dirname, '/chat-frontend/build/index.html'))
-);
-
 app.get('/rooms', (req, res) => {
-  res.json(rooms);
+  res.send(rooms);
 });
+
+
 
 async function getLastMessagesFromRoom(room) {
   //create a group of messages for each date
@@ -40,7 +36,7 @@ async function getLastMessagesFromRoom(room) {
     { $match: { to: room } },
     { $group: { _id: '$date', messagesByDate: { $push: '$$ROOT' } } },
   ]);
-
+  
   return roomMessages;
 }
 
@@ -55,10 +51,10 @@ function sortRoomMessagesByDate(messages) {
   return messages.sort(function (a, b) {
     let date1 = a._id.split('/');
     let date2 = b._id.split('/');
-
+    
     date1 = date1[2] + date1[1] + date1[0];
     date2 = date2[2] + date2[1] + date2[0];
-
+    
     return date1 < date2 ? -1 : 1;
   });
 }
@@ -68,22 +64,22 @@ io.on('connection', (socket) => {
   // ****** NEW USER *****
   socket.on('new-user', async () => {
     const members = await User.find();
-
+    
     //broadcast
     io.emit('new-user', members);
   });
-
+  
   // ****** JOIN ROOM *****
   socket.on('join-room', async (newRoom, previousRooms) => {
     socket.leave(previousRooms);
     socket.join(newRoom);
     let roomMessages = await getLastMessagesFromRoom(newRoom);
     roomMessages = sortRoomMessagesByDate(roomMessages);
-
+    
     // send to only specific user
     socket.emit('room-messages', roomMessages);
   });
-
+  
   // ****** GET MESSAGES TO ROOM *****
   socket.on('message-room', async (room, content, sender, time, date) => {
     const newMessage = await Message.create({
@@ -99,7 +95,7 @@ io.on('connection', (socket) => {
     io.to(room).emit('room-messages', roomMessages);
     socket.broadcast.emit('notifications', room);
   });
-
+  
   //logout
   app.delete('/logout', async (req, res) => {
     try {
@@ -108,7 +104,7 @@ io.on('connection', (socket) => {
       user.status = 'offline';
       user.newMessages = newMessages;
       await user.save();
-
+      
       const members = await User.find();
       socket.broadcast.emit('new-user', members);
       res.status(200).send();
@@ -118,6 +114,12 @@ io.on('connection', (socket) => {
     }
   });
 });
+
+const dirname = path.resolve();
+app.use(express.static(path.join(dirname, '/chat-frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(dirname, '/chat-frontend/build/index.html'))
+);
 
 server.listen(PORT, () => {
   console.log('listening to port', PORT);
